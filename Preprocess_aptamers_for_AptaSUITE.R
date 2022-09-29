0ae7656348baea502513bb0b670f45937d63f437
Li-Chin Wang <87052285+Lilian0296@users.noreply.github.com>
2022-09-29 16:57:52 +0800

    Add files via upload

---
+++ Preprocess_aptamers_for_AptaSUITE.R
@@ -0,0 +1,107 @@
+library(Biostrings)
+library(fuzzywuzzyR)
+library(QuasR)
+library(tidyr)
+library(dplyr)
+library("optparse")
+
+
+option_list = list(make_option("--output", type = "character", default = NULL, help  = "The path of output files"),
+                   make_option("--data", type = "character", default = NULL, help  = "The path of data folder (fastq files)"))  
+
+
+args <- parse_args(OptionParser(option_list=option_list))
+
+pathout<-args$outpath
+
+dir.create(paste(pathout,"/raw",sep=""))
+dir.create(paste(pathout,"/Clean_data",sep=""))
+
+# Merge fastq -------------------------------------
+message("Merge fastq files")
+
+datafolder <-args$data
+files=list.files(datafolder)
+message(files," will be preocessed")
+
+merge_fastq=DNAStringSet()
+
+for (i in 1:length(files)){
+  if (endsWith(files[i], "fastq.gz")){
+    data_path=paste(datafolder,"/",files[i],sep="")
+    print(paste("read ",files[i],sep=""))
+    data=readDNAStringSet(data_path,format="fastq",with.qualities=FALSE)
+    merge_fastq=append(merge_fastq,data)
+    #append(path)
+  }else{
+    message("Skip non-fastq files")
+  }
+}
+
+df <- as.data.frame(merge_fastq)
+# Read datatable----------------------------
+
+message("Read Primers data")
+Primer <- read.csv(paste(pathout,"/Primer.csv",sep = ''))
+message("Read Barcodes data")
+Barcodes <- read.csv(paste(pathout,"/Barcodes.csv",sep = ''))
+
+Primer=Primer %>% rowwise() %>% mutate(Sequences_reverse=as.character(reverseComplement(DNAString(Sequences))))
+Barcodes=Barcodes%>% rowwise() %>% mutate(Search_seed=paste(Sequences,Primer$Sequences[1],sep=("")))
+
+message("Process forward and reverse reads")
+
+fd <- data.frame(agrep(Primer["Sequences"][1,],df$x, max.distance = 2, value = TRUE))
+rv <- data.frame(agrep(Primer["Sequences_reverse"][1,], df$x, max.distance = 2, value = TRUE))
+rv <- data.frame(reverseComplement(DNAStringSet(rv$agrep.Primer..Sequences_reverse...1.....df.x..max.distance...2..)))
+colnames(fd)<-"sequences"
+colnames(rv)<-"sequences"
+df=rbind(fd,rv)
+
+message("Split reads based on barcodes")
+
+for (i in 1:length(Barcodes$Search_seed)){
+  message(paste("Process ",Barcodes$Name[i],sep=""))
+  barcodes_data <-agrep(Barcodes$Search_seed[i],df$sequences, max.distance = 1, value = TRUE)
+  data=DNAStringSet(barcodes_data)
+  filename_fasta <- paste(pathout,"/raw/(+)_",Barcodes$Name[i],".fasta",sep = '')
+  writeXStringSet(data,filename_fasta,format='fasta')
+  filename_fasta_removeprimers <- paste(pathout,"/raw/(+)_removeprimers_",Barcodes$Name[i],".fasta",sep = '')
+  removeprimers <-QuasR::preprocessReads(filename_fasta,
+                                         outputFilename=filename_fasta_removeprimers,
+                                         minLength=30,Lpattern = Primer$Sequences[1],Rpattern = Primer$Sequences[2],
+                                         truncateEndBases = 3)
+  print(removeprimers)
+  clean_data=data.frame(readDNAStringSet(filename_fasta_removeprimers,format="fasta",with.qualities=FALSE))
+  colnames(clean_data)<-"sequences"
+  random_clean=data.frame(clean_data[-grep(Primer$Sequences[1],clean_data$sequences),])
+  random_clean=random_clean[-grep(Primer$Sequences[2],random_clean$clean_data..grep.Primer.Sequences.1...clean_data.sequences...),]
+  random_clean=DNAStringSet(random_clean)
+  Fakeid=sprintf("ID%07d", seq_len(length(random_clean)))
+  names(random_clean) <-Fakeid
+  writeXStringSet(random_clean,paste(pathout,"/Clean_data/(+)_",Barcodes$Name[i],".fastq",sep = ''),format='fastq')
+}
+
+
+
+
+
+
+# Supplementary-----------------------------------------------------------------
+## create dataframe------
+#Primer=data.frame(Primer=c("Primer_forward","Primer_reverse"),
+#                  Sequences=c("ACGCTCGGATGCCACTACAG","CTCATGGACGTGCTGGTGAC"))
+
+
+#Barcodes=data.frame(Name=c("P3_B1","B4_B2","P6_B3","P7_B4","P9_B5","P10_B6","C8_B7","C9_B8"),
+#                    Sequences=c("CATGTCA",
+#                                "GTAAAGTCAA",
+#                                "CGGT",
+#                                "AACAGTTG",
+#                                "GACTG",
+#                                "CCATAAA",
+#                                "TTCCC",
+#                                "GAAGTCTA"))
+
+#write.csv(Primer,"D:/Lilian/Aptamer_4_2022/Preprocess aptamer for Aptasuit/Primer.csv",row.names=FALSE)
+#write.csv(Barcodes,"D:/Lilian/Aptamer_4_2022/Preprocess aptamer for Aptasuit/Barcodes.csv",row.names=FALSE)
